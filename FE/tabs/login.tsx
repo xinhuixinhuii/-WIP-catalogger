34e846e98316fda73d5c8406a337386ebe179c7d
xhxhxh <65407862+xinhuixinhuii@users.noreply.github.com>
2023-04-19 00:32:42 +0800

    [FE] setup login page

--- FE/tabs/login.tsx
+++ FE/tabs/login.tsx
@@ -1,12 +1,31 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Button, Text, TextInput, View } from 'react-native';
 
 const LoginScreen = () => {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Login!</Text>
-      </View>
-    );
-  }
+  const [user, setUser] = React.useState<string>('')
+  const [password, setPassword] = React.useState<string>('')
+
+  const onLogin = () => Alert.alert(user, password)
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TextInput 
+        placeholder='Username'
+        onChangeText={(user) => setUser(user)}
+        style = {{borderWidth: 1, marginBottom: 10, paddingHorizontal: 10}}
+      />
+      <TextInput 
+        placeholder='Password'
+        onChangeText={(password) => setPassword(password)}
+        style = {{borderWidth: 1, marginBottom: 10, paddingHorizontal: 10}}
+        secureTextEntry = {true}
+      />
+      <Button 
+        title='Login'
+        onPress={onLogin}
+      /> 
+    </View>
+  );
+}
 
 export default LoginScreen
