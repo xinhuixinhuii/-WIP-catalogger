bc1c199bfe7c29f8963ffed76b1e74ca5ffcc91b
xhxhxh <65407862+xinhuixinhuii@users.noreply.github.com>
2023-05-07 00:08:25 +0800

    [FE] delete navigation tab and maps

--- FE/App.tsx
+++ FE/App.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './tabs/home';
 import CatListScreen from './tabs/catList';
 import LoginScreen from './tabs/login';
 import CatLogScreen from './tabs/catLogs';
+import { Text } from 'react-native';
 
 const App = () => {
 
-  const Tab = createBottomTabNavigator();
+  // const Tab = createBottomTabNavigator();
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Cat Logs" component={CatLogScreen} />
-        <Tab.Screen name="Cat List" component={CatListScreen} />
-        <Tab.Screen name="Login" component={LoginScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Text>it works!</Text>
+    // <NavigationContainer>
+    //   <Tab.Navigator>
+    //     <Tab.Screen name="Home" component={HomeScreen} />
+    //     <Tab.Screen name="Cat Logs" component={CatLogScreen} />
+    //     <Tab.Screen name="Cat List" component={CatListScreen} />
+    //     <Tab.Screen name="Login" component={LoginScreen} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
   );
 };
 
